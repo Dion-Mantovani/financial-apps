bd6ba83604255542f56acbb0292021c6a3a7f8f4
Muhamad Dion Mantovani <studion.agency@gmail.com>
2026-09-13 09:40:04 +0700

    reset all features

--- src/lib/auth.ts
+++
@@ -1,2 +0,0 @@
-// Tempat simpan Dummy User ID sementara
-export const DUMMY_USER_ID = '9741711f-b8b0-407e-9cff-649867db6bc2';
--- src/services/categories.service.ts
+++
@@ -1,7 +0,0 @@
-import * as categoryQuery from '../query/categories.query';
-
-export const getCategories = async () => {
-  const { data, error } = await categoryQuery.fetchCategories();
-  if (error) throw new Error(error.message);
-  return data;
-};
